9434af6f81ac14057cb5c469367a9769ec8457b4
Felipe Amaral <pulga.lipe@gmail.com>
2021-05-31 19:47:05 -0300

    Added react-fast-refresh to make it easier to debug the application

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,25 +1,15 @@
-const repositoryName = 'unform2'
+import { RepositoryItem } from "./RepositoryItem"
+
 
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Form in React</p>
-          <a href="">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Form in React</p>
-          <a href="">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Form in React</p>
-          <a href="">Acessar repositório</a>
-        </li>
+       <RepositoryItem repository="unform2" />
+       <RepositoryItem />
+       <RepositoryItem />
+       <RepositoryItem />
       </ul>
     </section>
   )
