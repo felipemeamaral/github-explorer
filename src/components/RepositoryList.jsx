85e1ad4537bf3639470de51cad0ee073526f139a
Felipe Amaral <pulga.lipe@gmail.com>
2021-06-01 10:15:05 -0300

    Added styles to list elements

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,15 +1,16 @@
-import { RepositoryItem } from "./RepositoryItem"
+import { RepositoryItem } from './RepositoryItem'
 
+import '../styles/repositories.scss'
 
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-       <RepositoryItem repository="unform2" />
-       <RepositoryItem />
-       <RepositoryItem />
-       <RepositoryItem />
+        <RepositoryItem repository="unform" />
+        <RepositoryItem repository="unform" />
+        <RepositoryItem repository="unform" />
+        <RepositoryItem repository="unform" />
       </ul>
     </section>
   )
